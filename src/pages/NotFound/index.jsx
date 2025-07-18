4c82fed5530c065b45a45ada29693fe16d732c7b
Bruno Dück <bdfsilva@mppr.mp.br>
2025-07-18 12:38:34 -0300

    Editada a página de Notfound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -10,40 +10,7 @@ const status = 4
 
 function NotFound(){
     return (
-        <StepsContainer>
-            <Step 
-                title="Etapa 1"
-                done={status >= 1 ? true : false}
-                active={status === 1 ? true : false}
-                status={status}
-            />
-            <Step 
-                title="Etapa 2"
-                done={status >= 2 ? true : false}
-                active={status === 2 ? true : false}
-                status={status}
-            />
-            <Step 
-                title="Etapa 3"
-                description="Descrição da etapa 3"
-                done={status >= 3 ? true : false}
-                active={status === 3 ? true : false}
-                status={status}
-            />
-            <Step 
-                title="Etapa 4"
-                done={status >= 4 ? true : false}
-                active={status === 4 ? true : false}
-                status={status}
-            />
-            <Step 
-                title="Etapa 5"
-                done={status >= 5 ? true : false}
-                active={status === 5 ? true : false}
-                last={true}
-                status={status}
-            />
-        </StepsContainer>    
+        <h4>Ops! Endereço não encontrado</h4>  
     )
 }
 
