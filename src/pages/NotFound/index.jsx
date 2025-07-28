34a92412a2a3c10c89ce6197e4ccb787ad928a21
Bruno Dück <bdfsilva@mppr.mp.br>
2025-07-28 16:29:13 -0300

    Instalada a biblioteca axios

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,13 +1,3 @@
-import styled from "styled-components"
-import Step from "./step"
-
-const StepsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
-const status = 4
-
 function NotFound(){
     return (
         <h4>Ops! Endereço não encontrado</h4>  
